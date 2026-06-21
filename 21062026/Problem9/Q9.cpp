de598222441a01de6dbd6bbaed75e1576464b9ec
minhquan13-hub <dangminhquan130612@gmail.com>
2026-06-21 14:28:46 +0700

    feat: update Q9

--- 21062026/Problem9/Q9.cpp
+++ 21062026/Problem9/Q9.cpp
@@ -2,8 +2,8 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-bool check(long long x) {
-	for(long long i = 2; i*i<=x ;i++)
+bool check(long long x) { // kiem tra so nguyen to
+	for(long long i = 2; i*i<=x ;i++) // duyet vong for
 		if(x%i==0) return false;
 		return x > 1 ;
 }
@@ -13,18 +13,20 @@ int main() {
 	cin.tie(0);cout.tie(0);
 	cin >> n;
 	
-	long long k = n+1;	if(k<2) {
+	long long k = n+1;	
+	if(k<2) { // neu k<2 in ra -1 va ket thuc
 		cout << -1;
 		return 0;
 	}
 
-	while(true) {
-		if(check(k)==true) {
-			cout << abs(k-n) << "\n";
-			break;
+
+	while(true) { // duyet vong for tu n+1 den khi k hop le
+		if(check(k)==true) { // kiem tra k co phai so nguyen to k
+			cout << abs(k-n) << "\n"; // in ra gia tri tuyet doi cua k va n neu k la snt
+			break; // dung vong lap khi k la snt
 		}
-		k++;
+		k++; // tang bien k neu k co so nguyen to phu hop
 	}
-	return 0;
+	return 0; // ket thuc
 
 }
